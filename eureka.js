2e037e10278f9b59cc54259e3509de5652eb21e7
astenstrasser <astenstrasser@gmail.com>
2020-05-27 18:38:36 -0300

    calls exchange rates api

--- eureka.js
+++ eureka.js
@@ -1,25 +1,46 @@
 require('dotenv').config();
+const axios = require('axios');
+require('telegraf/extra')
+const Markup = require('telegraf/markup')
 
 console.log('Running Bot!')
 
 const Telegraf = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+bot.start(ctx => ctx.reply(`Bom dia, ${ctx.message.from.first_name}!`, Markup.inlineKeyboard([
+    // Markup.callbackButton("/start: start conversation with Eureka", "/start"),
+    Markup.callbackButton("EUR-BLR exchange rate", "euro")
+]).extra()));
 
-bot.start(ctx => ctx.reply(`Hey ${ctx.message.from.first_name}!`));
-
-bot.help(ctx => { ctx.reply('Some help message') });
+bot.action("euro", (ctx) => {
+    getEuroRates(ctx)
+});
 
 bot.on('sticker', (ctx) => {
     console.log(ctx.message.from.first_name)
     return ctx.reply('I could also send a sticker, but I won\'t')
 });
 
-bot.hears('euro', (ctx) => {
-    console.log(ctx)
-    return ctx.reply('I can\'t answer that yet.')
-});
+function getEuroRates(ctx) {
+    const requestUrl = `${process.env.EXCHANGE_RATE_API_URL}/${process.env.EXCHANGE_RATE_API_KEY}/latest/EUR`
 
-bot.hears(/([a-z])/, ({ reply }) => { reply('Invalid command. Type /help for my list of commands.') });
+    const euro = async() => {
+        let res = await axios.get(requestUrl)
+        ctx.reply(`Euro hoje: R$${res.data.conversion_rates.BRL}`, Markup.inlineKeyboard([
+            // Markup.callbackButton("/start: start conversation with Eureka", "/start"),
+            Markup.callbackButton("EUR-BLR exchange rate", "euro")
+        ]).extra());
+    }
+
+    return euro()
+}
+
+bot.hears(/([a-z])/, ({ reply }) => {
+    reply('Invalid command. Try one of these:', Markup.inlineKeyboard([
+        // Markup.callbackButton("/start: start conversation with Eureka", "/start"),
+        Markup.callbackButton("EUR-BLR exchange rate", "euro")
+    ]).extra());
+});
 
 bot.launch();
